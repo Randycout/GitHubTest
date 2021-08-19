f5b1a23e994ef6ee9b90ea4b05dee6c691fda782
randycout <2891651056@qq.com>
2021-08-19 22:30:32 +0800

    init:初始化GitHub项目

---
+++ main/java/com/chen/App.java
@@ -0,0 +1,14 @@
+package com.chen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("我是技术负责人");
+        System.out.println( "Hello World!" );
+    }
+}
